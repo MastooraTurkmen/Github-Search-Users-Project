3bf732da4355e5307cd7408f38839810b4f90bdd
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-19 18:16:38 +0430

    set up Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <h1>404</h1>
+      <h3>sorry the page you tried cannot be found</h3>
+      <Link to='/' className='btn'>Back home</Link>
+    </Wrapper>
+  )
 };
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
