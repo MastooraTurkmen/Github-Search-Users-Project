80cc771367b7221e153fee6c05d0459f1c1ca78c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-20 17:24:25 +0430

    set up user profile

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 import Card from './Card';
 import Followers from './Followers';
 const User = () => {
-  return <h2>search component</h2>;
+  return (
+    <setion className="section">
+      < Wrapper className='section-center'>
+        <Card></Card>
+        <Followers></Followers>
+      </Wrapper >
+    </setion>
+  )
 };
 
 const Wrapper = styled.div`
