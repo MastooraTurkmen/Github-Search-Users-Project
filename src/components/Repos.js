079825547ebcf80c3f44e4f52096592bbd860531
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-20 19:08:49 +0430

    react fusion chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,8 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext)
+  console.log(repos);
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <ExampleChart />
+      </Wrapper>
+    </section>
+  )
+
 };
 
 const Wrapper = styled.div`
