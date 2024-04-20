63be46876f0ad06eea1755cea7db9666e2da7b64
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-20 19:09:32 +0430

    rename

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -9,13 +9,13 @@ import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
 
 // Include the chart type
-import Column2D from "fusioncharts/fusioncharts.charts";
+import Chart from "fusioncharts/fusioncharts.charts";
 
 // Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
 // Adding the chart and theme as dependency to the core fusioncharts
-ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
 // STEP 2 - Chart Data
 const chartData = [
