47583b2455a404c3ebf35155c6a283e4953854fb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-21 17:51:47 +0430

    set up

--- src/context/context.js
+++ src/context/context.js
@@ -11,10 +11,31 @@ const GithubContext = React.createContext();
 const GithubProvider = ({ children }) => {
     const [githubUser, setGithubUser] = useState(mockUser)
     const [repos, setRepos] = useState(mockRepos)
-    const [followers, setFollowers] = useState(mockFollowers)
+    const [followers, setFollowers] = useState(mockFollowers);
+    const [requests, setRequests] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({ show: false, msg: '' })
+
+    const checkRequests = () => {
+        axios(`${rootUrl}/rate_limit`)
+            .then(({ data }) => {
+                let { rate: { remaining } } = data
+                setRequests(remaining)
+                if (remaining === 0) {
+                    handleError(true, 'Sorry, you have exceed your hourly limit')
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+    function handleError(show, msg) {
+        setError({ show, msg })
+    }
+
+    useEffect(checkRequests, [])
 
     return (
-        <GithubContext.Provider value={{ githubUser, repos, followers }}>
+        <GithubContext.Provider value={{ githubUser, repos, followers, requests, error }}>
             {children}
         </GithubContext.Provider>
     )
