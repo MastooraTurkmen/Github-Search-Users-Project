6ea796a8fc7cb75748455d47b408cdcd57030372
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-21 20:12:15 +0430

    search user

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,6 +4,18 @@ import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 
 const Dashboard = () => {
+  const { isLoading } = React.useContext(GithubContext)
+
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} alt="loading" className='loading-img' />
+      </main>
+    )
+  }
+
   return (
     <main>
       <Navbar />
