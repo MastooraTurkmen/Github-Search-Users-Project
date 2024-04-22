34c74835d7a70894c1990c154136280b3eda9c31
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-22 21:16:05 +0430

    auth user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,17 @@ import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
-  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } = useAuth0();
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0();
   const isUser = isAuthenticated && user;
 
   return (
     <Wrapper>
       {isUser && user.picture && <img src={user.picture} alt={user.name} />}
-      <button onClick={() => loginWithRedirect()}>login </button>
-      <button onClick={() => { logout({ returnTo: window.location.origin }) }}>logout </button>
+      {isUser && user.name && <h4>Welcome, <strong>{user.name.toUpperCase()}</strong></h4>}
+      {isUser ? (<button onClick={() => { logout({ returnTo: window.location.origin }) }}>logout </button>
+      ) : (
+        <button onClick={loginWithRedirect}>login </button>
+      )}
     </Wrapper>
   )
 };
