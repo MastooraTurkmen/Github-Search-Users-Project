79d2650559fbaaae3bde20e2a15c6a2948af6f39
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-22 21:55:30 +0430

    set up

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,11 +5,15 @@ import { Link } from 'react-router-dom';
 const Error = () => {
   return (
     <Wrapper>
-      <h1>404</h1>
-      <h3>sorry the page you tried cannot be found</h3>
-      <Link to='/' className='btn'>Back home</Link>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the page you tried cannot be found</h3>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </div>
     </Wrapper>
-  )
+  );
 };
 
 const Wrapper = styled.section`
