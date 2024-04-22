2ea74ec9cf060c5f80a5d25f31502fa7c0a8cd0f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-22 21:55:36 +0430

    auth user

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,16 +1,14 @@
-
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children }) => {
-  const { isAuthenticated, user } = useAuth0()
+  const { isAuthenticated, user } = useAuth0();
   const isUser = isAuthenticated && user;
-
   if (!isUser) {
-    return <Navigate to='/login' />
+    return <Navigate to='/login' />;
   }
-
   return children;
 };
+
 export default PrivateRoute;
