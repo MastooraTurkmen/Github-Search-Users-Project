6b5ced13846d4b5ee813f292170f486c37af85b5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-22 21:50:52 +0430

    set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -8,12 +7,15 @@ function App() {
     <AuthWrapper>
       <Router>
         <Routes>
-          <Route path='/' element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          } />
-          <Route path='/login' element={<Login />} />
+          <Route
+            path='/'
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path='login' element={<Login />} />
           <Route path='*' element={<Error />} />
         </Routes>
       </Router>
