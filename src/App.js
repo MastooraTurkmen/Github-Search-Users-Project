2c108f2b9983af8ce58a825d11778b9a61c20e05
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-22 21:15:50 +0430

    auth user

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
+
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <AuthWrapper>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Dashboard />} />
+          <PrivateRoute path='/' element={<Dashboard />} />
           <Route path='/login' element={<Login />} />
           <Route path='*' element={<Error />} />
         </Routes>
       </BrowserRouter>
-      {/* <Dashboard></Dashboard>
-      <Login />
-      <Error /> */}
-    </div>
+    </AuthWrapper>
   );
 }
 
